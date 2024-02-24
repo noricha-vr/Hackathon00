0bf0af63df9e34ecbd30e56d063a977c9557ea27
noricha-vr <noricha.vr@gmail.com>
2024-02-24 14:25:00 +0900

    Refactor game logic and add stage counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,27 @@
 <script>
     let timer = 0;
-    let deadline = 5; // 5秒後にデッドライン
-    let isGameOver = false;
+    let deadline = 5;
     let score = 0;
+    let isGameOver = false;
+    let isRunning = false;
     let intervalId;
     let waitTimeIntervalId;
-    let isRunning = false;
-    function startGame() {
+    let stage = 1;
+
+    function showAllLogs() {
+        console.log(
+            "isRunning:",
+            isRunning,
+            "isGameOver:",
+            isGameOver,
+            "timer:",
+            timer,
+            "score:",
+            score,
+        );
+    }
+    async function startGame() {
+        showAllLogs();
         if (isRunning) return;
         if (isGameOver) {
             resetGame();
@@ -16,37 +31,30 @@
         }
         const waitTime = Math.random() * 1000 + 1000;
         // ゲーム開始前の待機時間を設定
-        waitTimeIntervalId = setTimeout(() => {
-            if (!isRunning) return;
-            // タイマーを開始し、一定間隔で時間を更新
-            intervalId = setInterval(() => {
-                if (timer < deadline) {
-                    // デッドラインに達していない場合、タイマーを更新
-                    timer += 0.01;
-                } else {
-                    // デッドラインに達した場合、タイマーを停止しゲームオーバー状態にする
-                    gameOver();
-                }
-            }, 10); // 10ミリ秒ごとにタイマーを更新
-        }, waitTime); // ゲーム開始前の待機時間
+        await new Promise(
+            (resolve) => (waitTimeIntervalId = setTimeout(resolve, waitTime)),
+        ); // ゲーム開始前の待機時間
+        if (!isRunning) return;
+        // タイマーを開始し、一定間隔で時間を更新
+        intervalId = setInterval(() => {
+            if (timer < deadline) {
+                // デッドラインに達していない場合、タイマーを更新
+                timer += 0.01;
+            } else {
+                // デッドラインに達した場合、タイマーを停止しゲームオーバー状態にする
+                gameOver();
+            }
+        }, 10); // 10ミリ秒ごとにタイマーを更新
     }
 
     function go() {
-        console.log(
-            "isRunning:",
-            isRunning,
-            "isGameOver:",
-            isGameOver,
-            "timer:",
-            timer,
-        );
-        if (!isRunning) {
-            return;
-        } else if (timer === 0) {
+        showAllLogs();
+        if (timer === 0) {
             gameOver();
         } else {
             stop();
             score = deadline - timer;
+            stage = stage + 1;
         }
     }
     function stop() {
@@ -69,6 +77,7 @@
     function resetGame() {
         timer = 0;
         score = 0;
+        stage = 1;
         isGameOver = false;
         intervalId = null;
         isRunning = false;
@@ -77,11 +86,12 @@
 
 <section class="flex justify-center items-center bg-gray-100 my-10 p-5">
     <div class="text-4xl">
+        <div>第{stage}ステージ</div>
         <div>スコア: {score.toFixed(2)}</div>
         <div>タイマー: {timer.toFixed(2)}</div>
         {#if !isGameOver}
             {#if !isRunning}
-                <button on:click={startGame}>ゲーム開始</button>
+                <button on:click={startGame}>開始</button>
             {:else}
                 <button on:click={go}>ダッシュ！</button>
             {/if}
