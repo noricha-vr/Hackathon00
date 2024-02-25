71ed5172f29bcf6d82d0671793d78594e86927a2
noricha-vr <noricha.vr@gmail.com>
2024-02-25 17:28:26 +0900

    Add maxScore functionality and game instructions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
     const initialDeadline = 1.3;
     let deadline = initialDeadline;
     let score = 0;
+    let maxScore = 0;
     let isGameOver = false;
     let isRunning = false;
     let intervalId;
@@ -51,6 +52,12 @@
 
     onMount(() => {
         soundManager = new SoundManager();
+        // localStorageから最高スコアを読み込む
+        const storedMaxScore = localStorage.getItem("maxScore");
+        console.log("storedMaxScore", storedMaxScore);
+        if (storedMaxScore) {
+            maxScore = Number(storedMaxScore);
+        }
     });
     $: if (stage > 1) {
         policeImage = selectOne(policeImages);
@@ -79,7 +86,7 @@
             isRunning = true;
             timer = 0;
         }
-        const waitTime = Math.random() * 1000 + 1000;
+        const waitTime = Math.random() * 2000 + 1000;
         // ゲーム開始前の待機時間を設定
         await new Promise(
             (resolve) => (waitTimeIntervalId = setTimeout(resolve, waitTime)),
@@ -130,6 +137,12 @@
         isRunning = false;
         clearInterval(intervalId);
         clearInterval(waitTimeIntervalId);
+        // 現在のスコアが最高スコアより高ければ更新
+        if (score > maxScore) {
+            maxScore = score;
+            // 更新した最高スコアをlocalStorageに保存
+            localStorage.setItem("maxScore", maxScore.toString());
+        }
     }
 
     function resetGame() {
@@ -145,73 +158,94 @@
 
 <section class="flex justify-center items-center bg-gray-100 p-5">
     <div class="max-w-lg text-center">
+        <div class="mt-5 p-2 pb-5 bg-blue-100 rounded-lg shadow-md mb-6">
+            <div class="text-xl mb-3 mt-3 font-bold">遊び方</div>
+            <ul class="list-inside space-y-2">
+                <li>
+                    <span class="text-blue-500 font-bold">青信号</span> の時にダッシュボタンを押してスコアを稼ごう！
+                </li>
+                <li>
+                    <span class="text-red-500 font-bold">赤信号</span> の時にダッシュボタンを押すとゲームオーバー！
+                </li>
+                <li>ステージが上がると難易度が上がって、スコアも高くなるよ</li>
+            </ul>
+        </div>
+
         <div class="p-5 bg-white rounded-lg shadow-lg mt-5 mb-10">
+            <div class="text-left mb-3 p-3">
+                <div>
+                    最高スコア: {Number(maxScore.toFixed(0)) * 10}
+                </div>
+                <div class="">
+                    スコア: {Number(score.toFixed(0)) * 10}
+                </div>
+                <div class="mb-3">タイマー: {timer.toFixed(2)}</div>
+            </div>
             <div class="text-4xl mb-3">第{stage}ステージ</div>
             <!-- <div>デッドライン: {deadline.toFixed(2)}</div> -->
-            <div class="text-2xl">スコア: {Number(score.toFixed(0)) * 10}</div>
-            <!-- <div>タイマー: {timer.toFixed(2)}</div> -->
-        </div>
-        <div class="text-2xl">
-            <img
-                class="w-1/2 mx-auto text-center mb-5"
-                alt="信号機"
-                src={`/image/traffic-light/${trafficLight}.png`}
-                transition:slide={{ duration: 500 }}
-            />
-            {#if !isGameOver}
-                {#if !isRunning}
-                    <button
-                        class="bg-blue-500 text-white py-2 px-4 rounded"
-                        on:click={startGame}>開始</button
-                    >
-                    {#if stage === 1}
-                        <img
-                            class="w-1/2 mx-auto text-center"
-                            alt="待機中の車"
-                            src={idlingCarImage}
-                        />
+
+            <div class="text-2xl">
+                <img
+                    class="w-1/2 mx-auto text-center mb-5"
+                    alt="信号機"
+                    src={`/image/traffic-light/${trafficLight}.png`}
+                    transition:slide={{ duration: 500 }}
+                />
+                {#if !isGameOver}
+                    {#if !isRunning}
+                        <button
+                            class="bg-blue-500 text-white py-2 px-4 rounded"
+                            on:click={startGame}>開始</button
+                        >
+                        {#if stage === 1}
+                            <img
+                                class="w-1/2 mx-auto text-center"
+                                alt="待機中の車"
+                                src={idlingCarImage}
+                            />
+                        {:else}
+                            <img
+                                class="mx-auto"
+                                alt="茨城ダッシュ"
+                                src={speedingCarImage}
+                            />
+                        {/if}
                     {:else}
+                        <button
+                            class="bg-blue-500 text-white py-2 px-4 rounded"
+                            on:click={go}>ダッシュ！</button
+                        >
                         <img
-                            class="mx-auto"
-                            alt="茨城ダッシュ"
-                            src={speedingCarImage}
+                            class="w-1/2 mx-auto"
+                            alt="待機"
+                            src={idlingCarImage}
                         />
                     {/if}
                 {:else}
+                    <div class="text-6xl font-bold text-red-500">
+                        ゲームオーバー
+                        {#if trafficLight === "red"}
+                            <!-- おまわりさんが出てくる -->
+                            <img
+                                class="w-1/3 mx-auto"
+                                src={policeImage}
+                                alt="おまわりさん"
+                            />
+                        {:else}
+                            <!-- 衝突事故 -->
+                            <img
+                                class="w-1/2 mx-auto"
+                                src={accidentImage}
+                                alt="衝突事故"
+                            />
+                        {/if}
+                    </div>
                     <button
                         class="bg-blue-500 text-white py-2 px-4 rounded"
-                        on:click={go}>ダッシュ！</button
+                        on:click={startGame}>リトライ</button
                     >
-                    <img
-                        class="w-1/2 mx-auto"
-                        alt="待機"
-                        src={idlingCarImage}
-                    />
                 {/if}
-            {:else}
-                <div class="text-6xl font-bold text-red-500">
-                    ゲームオーバー
-                    {#if trafficLight === "red"}
-                        <!-- おまわりさんが出てくる -->
-                        <img
-                            class="w-1/3 mx-auto"
-                            src={policeImage}
-                            alt="おまわりさん"
-                        />
-                    {:else}
-                        <!-- 衝突事故 -->
-                        <img
-                            class="w-1/2 mx-auto"
-                            src={accidentImage}
-                            alt="衝突事故"
-                        />
-                    {/if}
-                </div>
-                <button
-                    class="bg-blue-500 text-white py-2 px-4 rounded"
-                    on:click={startGame}>リトライ</button
-                >
-            {/if}
+            </div>
         </div>
     </div>
 </section>
