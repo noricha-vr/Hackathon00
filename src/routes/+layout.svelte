200c9ded85150c95ecf9e87ce659861e49d0cdb6
noricha-vr <noricha.vr@gmail.com>
2024-02-28 15:54:11 +0900

    Add MetaTags component and update layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,15 @@
     import "../app.css";
     import Footer from "$lib/components/Footer.svelte";
     import GoogleAnalytics from "$lib/components/GoogleAnalytics.svelte";
+    import MetaTags from "$lib/components/MetaTags.svelte";
+    const title = "バーチャルな茨城で茨城ダッシュ！";
+    const description =
+        "茨城を舞台にしたバーチャルなゲーム体験を提供するこのサイトでは、プレイヤーはバーチャルな茨城で茨城ダッシュを楽しむことができます。青信号になったらすぐにダッシュ！スコアを稼ぎながらステージを進めよう！赤信号でダッシュボタンを押すとゲームオーバーになるので気をつけてね！最高スコアを目指して、茨城ダッシュのスリルを楽しもう！";
+    const thumbnail = "/image/thumbnail.png";
 </script>
 
 <svelte:head>
+    <MetaTags {title} {description} {thumbnail} />
     <GoogleAnalytics />
 </svelte:head>
 
