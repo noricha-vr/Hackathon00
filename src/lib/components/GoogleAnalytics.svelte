25654d1929a59cb7f817fe8349aef6b453a9fa3e
noricha-vr <noricha.vr@gmail.com>
2024-02-28 13:36:03 +0900

    Update Google Analytics script URL

--- src/lib/components/GoogleAnalytics.svelte
+++ src/lib/components/GoogleAnalytics.svelte
@@ -1,11 +1,12 @@
 <script>
     import { onMount } from "svelte";
-    import { PUBLIC_GOOGLE_ANALYTICS_ID } from "$env/static/public";
 
     onMount(() => {
         // Google Analyticsのスクリプトを動的に追加
         const scriptUrl = document.createElement("script");
-        scriptUrl.src = `https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GOOGLE_ANALYTICS_ID}`;
+        scriptUrl.src = `https://www.googletagmanager.com/gtag/js?id=${
+            import.meta.env.VITE_PUBLIC_GOOGLE_ANALYTICS_ID
+        }`;
         scriptUrl.async = true;
         document.head.appendChild(scriptUrl);
 
@@ -14,6 +15,6 @@
             dataLayer.push(arguments);
         }
         gtag("js", new Date());
-        gtag("config", PUBLIC_GOOGLE_ANALYTICS_ID);
+        gtag("config", import.meta.env.VITE_PUBLIC_GOOGLE_ANALYTICS_ID);
     });
 </script>
