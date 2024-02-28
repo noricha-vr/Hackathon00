2061a3a632f9af03027a13e6d8623a6b781619ac
noricha-vr <noricha.vr@gmail.com>
2024-02-28 14:27:47 +0900

    Add process.env.PUBLIC_GOOGLE_ANALYTICS_ID to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [sveltekit()],
+    define: {
+        'process.env.PUBLIC_GOOGLE_ANALYTICS_ID': JSON.stringify(process.env.PUBLIC_GOOGLE_ANALYTICS_ID),
+    },
     server: {
         fs: {
             // プロジェクトのルートディレクトリからの相対パスを使用
