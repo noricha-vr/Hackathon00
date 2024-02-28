0de3c63b96e870b1b1fab349f7cc03872ae13b16
noricha-vr <noricha.vr@gmail.com>
2024-02-28 14:04:19 +0900

    Refactor Google Analytics script loading

--- src/lib/components/GoogleAnalytics.svelte
+++ src/lib/components/GoogleAnalytics.svelte
@@ -1,20 +1,19 @@
 <script>
+    import { PUBLIC_GOOGLE_ANALYTICS_ID } from "$env/static/public";
     import { onMount } from "svelte";
 
     onMount(() => {
-        // Google Analyticsのスクリプトを動的に追加
-        const scriptUrl = document.createElement("script");
-        scriptUrl.src = `https://www.googletagmanager.com/gtag/js?id=${
-            import.meta.env.VITE_PUBLIC_GOOGLE_ANALYTICS_ID
-        }`;
-        scriptUrl.async = true;
-        document.head.appendChild(scriptUrl);
+        const scriptTag = document.createElement("script");
+        scriptTag.src = `https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GOOGLE_ANALYTICS_ID}`;
+        scriptTag.async = true;
+        document.head.appendChild(scriptTag);
 
         window.dataLayer = window.dataLayer || [];
         function gtag() {
             dataLayer.push(arguments);
         }
         gtag("js", new Date());
-        gtag("config", import.meta.env.VITE_PUBLIC_GOOGLE_ANALYTICS_ID);
+
+        gtag("config", PUBLIC_GOOGLE_ANALYTICS_ID);
     });
 </script>
